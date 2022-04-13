9f74d552fb5c68ed14ece6c620e47e4ec5044618
Nuruzzamanshah <mnueuzzaman@gmail.com>
2022-04-13 07:18:35 -0700

    navigate hooks added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Checkout from './Pages/Checkout/Checkout/Checkout';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
@@ -8,6 +9,8 @@ import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 import NotFound from './Pages/Shared/NotFound/NotFound';
+import RequireAuth from './Pages/Login/RequirAuth/Requireauth';
+
 
 function App() {
   return (
@@ -20,6 +23,12 @@ function App() {
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/checkout' element={
+          <RequireAuth>
+            <Checkout></Checkout>
+          </RequireAuth>
+          
+        }></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,35 +1,49 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import './Register.css';
+import auth from '../../../firebase.init';
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
     const navigate = useNavigate('');
 
     const navigateLogin = () =>{
         navigate('/login')
     }
-    const handleRegister = event =>{
-        event.preventDefault();
-        // console.log(event.target.email.value);
-        const name  = event.target.name.value;
-        const email = event.target.email.value;
-        const password = event.target.password.value;
+    if(user){
+        navigate('/home')
     }
-    return (
-        <div className='register-from'>
-            <h2 className='text-center'>Please Register</h2>
-            <form onSubmit={handleRegister}>
-            <input type="text" id="fname" placeholder='Enter Your Name' name="fname"/>
+
+
+const handleRegister = event =>{
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    createUserWithEmailAndPassword(email, password);
+}
+
+return (
+    <div className='register-from'>
+        <h2 style={{textAlign: 'center'}}>Please Register</h2>
+        <form onSubmit={handleRegister}>
+            <input type="text" name="name" id="" placeholder='Your Name'/>
             
-            <input type="email" id="email" placeholder='enter Your Email' required name="email"/>
+            <input type="email" name="email" id="" placeholder='Email Address' required/>
             
-            <input type="password" id="pwd" placeholder='enter Your Password' required name="pwd"></input>
-    
-            <input type="submit" value="Register"></input>
-            </form>
-            <p>Already have An Account? <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
-        </div>
-    );
+            <input type="password" name="password" id="" placeholder='Password' required/>
+            <input type="submit" value="Register" />
+        </form>
+        <p>Already have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
+    </div>
+);
 };
 
 export default Register;
